0b1c25e314a47c6aef9a7c17c7ebdd7f625ecf54
boshraemad <boshraemad2004@icloud.com>
2025-07-22 04:12:12 +0300

    implementing main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter } from "react-router-dom"
+import { Routes , Route } from "react-router-dom"
+import HomePage from "./Pages/HomePage"
+import Product from "./Pages/Product"
+import Pricing from "./Pages/Pricing"
+import ErrorPage from "./Pages/ErrorPage"
+
 function App() {
   return (
-    <>
-     <div>worldwise</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
